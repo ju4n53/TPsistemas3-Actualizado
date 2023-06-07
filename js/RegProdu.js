9f36428accb820de78561dcf264395017666ca65
Juan <juansegutierrez7@gmail.com>
2023-06-07 03:50:49 -0300

    correcciones en RegEmple

--- js/RegProdu.js
+++ js/RegProdu.js
@@ -1,15 +1,18 @@
 let contador = 100;
 let codigo = [];
 let producto = [];
+let porcion=[];
 let precio = [];
 let i = 0;
 function carga1() {
 	//const _txtCodigo = document.getElementById("txtCodigo");
 	const _txtProducto = document.getElementById("txtProducto");
+	const _txtPorcion = document.getElementById("txtPorcion");
 	const _txtPrecio = document.getElementById("txtPrecio");
 
 	//codigo[i] = _txtCodigo.value;
 	producto[i] = _txtProducto.value;
+	porcion[i] = _txtPorcion.value;
 	precio[i] = parseInt(_txtPrecio.value);
 	i++;
 	contador = contador + 1;
@@ -21,12 +24,16 @@ function carga1() {
 	_td1.innerHTML = _txtProducto.value;
 
 	const _td2 = document.createElement("td");
-	_td2.innerHTML = _txtPrecio.value;
+	_td2.innerHTML = _txtPorcion.value;
+
+	const _td3 = document.createElement("td");
+	_td3.innerHTML = _txtPrecio.value;
 
 	const _tr = document.createElement("tr");
 	_tr.appendChild(_td0);
 	_tr.appendChild(_td1);
 	_tr.appendChild(_td2);
+	_tr.appendChild(_td3);
 
 	const _tbody = document.querySelector("tbody");
 	_tbody.appendChild(_tr);
@@ -34,6 +41,7 @@ function carga1() {
 
 	//_txtCodigo.value = "";
 	_txtProducto.value = "";
+	_txtPorcion.value = "";
 	_txtPrecio.value = "";
 	_txtProducto.focus();
 
--- js/RegProdu2.js
+++ js/RegProdu2.js
@@ -1,15 +1,18 @@
 let codigo2 = [];
 let bebida = [];
+let volumen =[];
 let precio2 = [];
 let e = 0;
 let contador2 = 300;
 function carga2() {
 	//const _txtCodigo2 = document.getElementById("txtCodigo2");
 	const _txtBebida = document.getElementById("txtBebida");
+	const _txtVolumen = document.getElementById("txtVolumen");
 	const _txtPrecio2 = document.getElementById("txtPrecio2");
 
 	//codigo2[e] = _txtCodigo2.value;
 	bebida[e] = _txtBebida.value;
+	volumen[e] = _txtVolumen.value;
 	precio2[e] = parseInt(_txtPrecio2.value);
 	e++;
 	contador2 = contador2 + 1;
@@ -21,18 +24,23 @@ function carga2() {
 	_td1.innerHTML = _txtBebida.value;
 
 	const _td2 = document.createElement("td");
-	_td2.innerHTML = _txtPrecio2.value;
+	_td2.innerHTML = _txtVolumen.value;
+
+	const _td3 = document.createElement("td");
+	_td3.innerHTML = _txtPrecio2.value;
 
 	const _tr = document.createElement("tr");
 	_tr.appendChild(_td0);
 	_tr.appendChild(_td1);
 	_tr.appendChild(_td2);
+	_tr.appendChild(_td3);
 
 	const _tbody2 = document.getElementById("tbody2");
 	_tbody2.appendChild(_tr);
 
 	
 	_txtBebida.value = "";
+	_txtVolumen.value = "";
 	_txtPrecio2.value = "";
 	_txtBebida.focus();
 
